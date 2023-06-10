4c1e3c801e68a940ca0daaeb0dc9878c44b5a63d
Yurii Kravchenko <u.kravchenko03@gmail.com>
2023-06-10 20:27:15 +1000

    added 16, 18,20

---
+++ DZ_16/DZ.py
@@ -0,0 +1,16 @@
+quantity=int(input('Количество элементов в массиве : '))
+value_to_find=int(input('Какое число искать : '))
+
+there_is = 0
+my_list = []
+
+for i in range(quantity):
+    my_list.insert(i,i+1)
+    if (my_list[i]==value_to_find):
+        there_is+=1
+
+print(my_list)
+print(there_is)
+
+
+
